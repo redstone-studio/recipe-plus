b35960371c37db356ed617d3b427212bbdcd214c
Axuata <axuata@gmail.com>
2024-08-29 14:33:14 +0900

    A

---
+++ src/client/java/org/redstonestudio/recipe_plus/client/RecipePlusClient.java
@@ -0,0 +1,10 @@
+package org.redstonestudio.recipe_plus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecipePlusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
